109e636e3d6af0b8a20fb846a914eb423525961e
DotChoco <chocoretostonus@gmail.com>
2024-09-09 13:08:00 -0600

    i: Repository Created

---
+++ NKDot.cs
@@ -0,0 +1,16 @@
+﻿
+namespace NKDot
+{
+    public static class NKObj
+    {
+        public static string NKRPT(string data)
+        {
+            return data;
+        }
+
+        public static string DKRPT(string data)
+        {
+            return data;
+        }
+    }
+}
